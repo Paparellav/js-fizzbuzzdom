72b26f9b566189eb4a5cf1755a4c38eda65c8faa
Paparellav <vitantoniopaparella@gmail.com>
2022-04-11 13:45:36 +0200

    step 1 completed

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Stampare i numeri da 1 a 100 in console
+for (let i = 1; i <= 100; i++) {
+
+    //1. Stabilire se il numero è divisibile per 3, per 5 o per entrambi
+    let numberX = i;
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        numberX = "FizzBuzz";
+    } else if (i % 3 === 0) {
+        numberX = "Fizz";
+    } else if (i % 5 === 0) {
+        numberX = "Buzz";
+    }
+
+    console.log(numberX);
+}
